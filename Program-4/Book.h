91c6421773cb40ec2fce4a1ff9670395616ce806
Nuradeen Sabha <nouradeen0@gmail.com>
2016-05-05 18:25:41 -0500

    Added files via upload

---
+++ Program-4/Book.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+struct bookItem
+{
+	int ID;
+	string Title;
+	string FName;
+	string MName;
+	string LName;
+};
